5fabc106ed1cccf050811ccba59fe75e3224c78f
YQM <132966105+Yaraqm@users.noreply.github.com>
2025-03-15 01:35:02 -0400

    Update time_server.c

--- time_server.c
+++ time_server.c
@@ -1,70 +1,69 @@
-/* time_server.c - main */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <time.h>
+#include <arpa/inet.h>
 
+int main(int argc, char *argv[]) {
+    struct sockaddr_in fsin;  /* Client address */
+    char buf[100];            /* Input buffer */
+    char *pts;                /* Pointer to time string */
+    int s;                    /* Server socket */
+    time_t now;               /* Current time */
+    socklen_t alen;           /* Address length */
+    struct sockaddr_in sin;   /* Server address */
+    int port = 3000;          /* Default port */
 
-/*------------------------------------------------------------------------
- * main - Iterative UDP server for TIME service
- *------------------------------------------------------------------------
- */
-int
-main(int argc, char *argv[])
-{
-	struct  sockaddr_in fsin;	/* the from address of a client	*/
-	char	buf[100];		/* "input" buffer; any size > 0	*/
-	char    *pts;
-	int	sock;			/* server socket		*/
-	time_t	now;			/* current time			*/
-	int	alen;			/* from-address length		*/
-	struct  sockaddr_in sin; /* an Internet endpoint address         */
-        int     s, type;        /* socket descriptor and socket type    */
-	int 	port=3000;
-                                                                                
+    /* Handle command-line arguments */
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    } else if (argc > 2) {
+        fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+        exit(1);
+    }
 
-	switch(argc){
-		case 1:
-			break;
-		case 2:
-			port = atoi(argv[1]);
-			break;
-		default:
-			fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-			exit(1);
-	}
+    /* Create a UDP socket */
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) {
+        perror("socket creation failed");
+        exit(1);
+    }
 
-        memset(&sin, 0, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_addr.s_addr = INADDR_ANY;
-        sin.sin_port = htons(port);
-                                                                                                 
-    /* Allocate a socket */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
-        if (s < 0)
-		fprintf(stderr, "can't creat socket\n");
-                                                                                
+    /* Configure server address */
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(port);
+
     /* Bind the socket */
-        if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
-		fprintf(stderr, "can't bind to %d port\n",port);
-        listen(s, 5);	
-	alen = sizeof(fsin);
+    if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
 
-	while (1) {
-		
-		if (recvfrom(s, buf, sizeof(buf), 0,
-				(struct sockaddr *)&fsin, &alen) < 0)
-			fprintf(stderr, "recvfrom error\n");
+    printf("UDP Time Server started on port %d...\n", port);
 
-		(void) time(&now);
-        	pts = ctime(&now);
+    alen = sizeof(fsin);
+    while (1) {
+        /* Receive message */
+        if (recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&fsin, &alen) < 0) {
+            perror("recvfrom error");
+            continue;
+        }
 
-		(void) sendto(s, pts, strlen(pts), 0,
-			(struct sockaddr *)&fsin, sizeof(fsin));
-	}
+        /* Get current time */
+        time(&now);
+        pts = ctime(&now);
+
+        /* Send response */
+        if (sendto(s, pts, strlen(pts), 0, (struct sockaddr *)&fsin, alen) < 0) {
+            perror("sendto error");
+        }
+    }
+
+    close(s);
+    return 0;
 }
